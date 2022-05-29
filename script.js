97f47e10df98824bf24a59e4ff57fb68b3eb4525
Boyarkov <boyarkov@gmail.com>
2022-05-29 20:45:26 +0300

    второе задание сложнное

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 let num = 266219;
+let array = num.toString().split('');
+let result = array.reduce((accumulator, currentValue) => accumulator * currentValue);
+console.log(result);
+console.log((result ** 3).toString().slice(0, 2));
