42498c969e5858809bc8fd36dffe321ed03f01da
Boyarkov <boyarkov@gmail.com>
2022-06-09 22:59:28 +0300

    четвертое задание

--- script.js
+++ script.js
@@ -1,25 +1,16 @@
 'use strict';
 
 let title = prompt('Название вашего проекта');
-
 let screens = prompt('Какие типы экранов нужно разработать?');
-
 let screenPrice = parseFloat(prompt('Сколько будет стоить данная работа?', '1000 руб.'));
-
 let adaptive = confirm('Нужен ли адаптив на сайте?');
-
 let service1 = prompt('Какой дополнительный тип услуги нужен?');
-
 let servicePrice1 = parseFloat(prompt('Сколько это будет стоить?', '1000 руб.'));
-
 let service2 = prompt('Какой дополнительный тип услуги нужен?');
-
 let servicePrice2 = parseFloat(prompt('Сколько это будет стоить?', '1000 руб.'));
-
-let fullPrice = screenPrice + servicePrice1 + servicePrice2;
-
 let rollback = 20;
-let servicePercentPrice = Math.ceil(fullPrice - rollback);
+let fullPrice = screenPrice + servicePrice1 + servicePrice2;
+let servicePercentPrice = Math.ceil(fullPrice - (fullPrice * (rollback / 100)));
 console.log(servicePercentPrice);
 
 switch (true) {
@@ -37,27 +28,3 @@ switch (true) {
         break;
 }
 
-/*let title = "NewProject";
-let screens = "Простые, Сложные, Интерактивные";
-let screenPrice = 10000;
-let rollback = 20;
-let fullPrice = 200000;
-let adaptive = true;
-
-
-console.log(typeof title);
-
-console.log(typeof fullPrice);
-
-console.log(typeof adaptive);
-
-console.log(screens.length);
-
-console.log('Стоимость верстки экранов ', screenPrice, ' рублей');
-
-console.log('Стоимость разработки сайта ', fullPrice, ' рублей');
-
-console.log(screens.toLowerCase().split(", "));
-
-console.log('Процент отката посреднику за работу ', fullPrice * (rollback / 100), ' рублей');
-*/
