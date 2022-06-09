1830d592f10ead810dca2c34c13a5d180601bf2d
Boyarkov <boyarkov@gmail.com>
2022-06-09 22:57:06 +0300

    исправления в третьем задании

--- script.js
+++ script.js
@@ -1,25 +1,16 @@
 'use strict';
 
 let title = prompt('Название вашего проекта');
-
 let screens = prompt('Какие типы экранов нужно разработать?');
-
 let screenPrice = parseFloat(prompt('Сколько будет стоить данная работа?', '1000 руб.'));
-
 let adaptive = confirm('Нужен ли адаптив на сайте?');
-
 let service1 = prompt('Какой дополнительный тип услуги нужен?');
-
 let servicePrice1 = parseFloat(prompt('Сколько это будет стоить?', '1000 руб.'));
-
 let service2 = prompt('Какой дополнительный тип услуги нужен?');
-
 let servicePrice2 = parseFloat(prompt('Сколько это будет стоить?', '1000 руб.'));
-
-let fullPrice = screenPrice + servicePrice1 + servicePrice2;
-
 let rollback = 20;
-let servicePercentPrice = Math.ceil(fullPrice - rollback);
+let fullPrice = screenPrice + servicePrice1 + servicePrice2;
+let servicePercentPrice = Math.ceil(fullPrice - (fullPrice * (rollback / 100)));
 console.log(servicePercentPrice);
 
 switch (true) {
